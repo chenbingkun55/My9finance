21e8ed7b413c6b3128f409c75d4191765b0e881f
chenbingkun55 <chenbingkun55@163.com>
2012-04-18 17:21:58 +0800

    修改为Public配置文件.

--- config.inc.php
+++ config.inc.php
@@ -1,8 +1,8 @@
 <?PHP
 
-define("UserName","recod");
+define("UserName","xth_10564221");
 define("PassWord","chenbk55");
-define("ServerName","localhost");
+define("ServerName","sql307.xtreemhost.com");
 define("DBName","my9finance");
 define("ERRORFILE","error.php");
 // define("ROOT_PATH",dirname(_FILE_).'/');
